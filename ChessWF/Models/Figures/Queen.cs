1139e0ada6ffc2f8dab721c3f7a5ff90ca0df552
Isakovskiy <rim0123@bk.ru>
2022-06-22 15:50:47 +0300

    created queen and done her moving

---
+++ ChessWF/Models/Figures/Queen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWF.Models.Figures
+{
+    internal class Queen : Figure
+    {
+        public Queen(string image, bool isBlack = false) : base(image, isBlack)
+        {
+        }
+
+        public override List<Sell> GetAvaibleSells(Sell figureSell, Sell[,] boardSells)
+        {
+            var list = new List<Sell>();
+            list.AddRange(new Rook(image: "fake").GetAvaibleSells(figureSell, boardSells));
+            list.AddRange(new Bishop(image: "fake").GetAvaibleSells(figureSell, boardSells));
+
+            return list;
+        }
+    }
+}
